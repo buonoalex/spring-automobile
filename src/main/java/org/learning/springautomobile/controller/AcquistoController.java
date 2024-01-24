a0adcdb3f1aa982a1584607e32d06b7310fe8a90
buonoalex <loopatthecity@gmail.com>
2024-01-24 16:25:46 +0100

    Add Controller,Repository,Model

---
+++ src/main/java/org/learning/springautomobile/controller/AcquistoController.java
@@ -0,0 +1,38 @@
+package org.learning.springautomobile.controller;
+
+import jakarta.validation.Valid;
+import org.learning.springautomobile.model.Acquisto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("home/auto/acquisto")
+public class AcquistoController {
+
+    @Autowired
+    private
+
+    @GetMapping
+    public String index(){
+        return "";
+    }
+
+    @GetMapping("/create")
+    public String createAcquisto(Model model){
+        Acquisto acquisto = new Acquisto();
+        model.addAttribute("acquisto",acquisto);
+        return "";
+    }
+
+    @PostMapping("/create")
+    public String saveAcquisto(@Valid @ModelAttribute("acquisto") Acquisto formAcquisto, BindingResult bindingResult){
+        return "";
+    }
+
+}
