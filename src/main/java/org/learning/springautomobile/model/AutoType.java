166f27ae694405364eed674d1fc76ea1a49ab0d8
xgiannix <gianni.demare@hotmail.it>
2024-01-24 16:26:16 +0100

    Aggiunto Model - Repository - Controller di AutoType

---
+++ src/main/java/org/learning/springautomobile/model/AutoType.java
@@ -0,0 +1,43 @@
+package org.learning.springautomobile.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+
+import java.util.List;
+
+@Entity
+@Table(name = "type")
+public class AutoType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotEmpty
+    private String descrizione;
+
+    @OneToMany(mappedBy = "autoType")
+    private List<Auto> autoList;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public List<Auto> getAutoList() {
+        return autoList;
+    }
+
+    public void setAutoList(List<Auto> autoList) {
+        this.autoList = autoList;
+    }
+}
