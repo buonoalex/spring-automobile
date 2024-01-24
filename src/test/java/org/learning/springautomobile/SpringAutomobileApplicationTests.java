54cbfad81bb98cd2654c56f05c052c3b02baa6b0
buonoalex <loopatthecity@gmail.com>
2024-01-24 11:39:35 +0100

    forza napoli

---
+++ src/test/java/org/learning/springautomobile/SpringAutomobileApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learning.springautomobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAutomobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
