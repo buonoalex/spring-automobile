9c26ab0fc1cb0c95542a52ac767b0644f2ab9885
buonoalex <loopatthecity@gmail.com>
2024-01-25 11:19:36 +0100

    fix bug 3

--- src/main/java/org/learning/springautomobile/repository/AcquistoRecovery.java
+++
@@ -1,7 +0,0 @@
-package org.learning.springautomobile.repository;
-
-import org.learning.springautomobile.model.Acquisto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AcquistoRecovery extends JpaRepository<Acquisto,Integer> {
-}
