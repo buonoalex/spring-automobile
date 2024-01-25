e96f47593a2bf8835ba6880676dffed6d6fdd906
buonoalex <loopatthecity@gmail.com>
2024-01-25 11:03:15 +0100

    Add AcquistoCliente and AcquistoRifornitore

--- src/main/java/org/learning/springautomobile/model/AcquistoCliente.java
+++ src/main/java/org/learning/springautomobile/model/AcquistoCliente.java
@@ -3,33 +3,31 @@ package org.learning.springautomobile.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Date;
-import java.util.Set;
 
 @Entity
-public class Acquisto {
+@Table(name = "acquisto_cliente")
+public class AcquistoCliente {
 
-
+    //Attributi
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @NotNull(message = "Questo campo non può essere vuoto")
+    @NotNull
+    @Column(nullable = false)
     private LocalDate data;
 
-    @NotNull(message = "La quantità non può essere 0")
+    @NotNull
+    @Column(nullable = false)
     private int quantità;
 
     @ManyToOne
     private Auto auto;
+
     //Costruttori
 
     //Metodi
-    public BigDecimal getPrezzo(){
-        return this.auto.getPrice();
-    }
 
     //Getter and Setter
     public int getId() {
@@ -48,19 +46,19 @@ public class Acquisto {
         this.data = data;
     }
 
-    public Auto getAuto() {
-        return auto;
-    }
-
-    public void setAuto(Auto auto) {
-        this.auto = auto;
-    }
-
     public int getQuantità() {
         return quantità;
     }
 
     public void setQuantità(int quantità) {
         this.quantità = quantità;
+    }
+
+    public Auto getAuto() {
+        return auto;
+    }
+
+    public void setAuto(Auto auto) {
+        this.auto = auto;
     }
 }
