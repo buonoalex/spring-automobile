65355c2424ad7b574e9be1022b6ae2e6f8a02cd5
buonoalex <loopatthecity@gmail.com>
2024-01-25 09:22:40 +0100

    add get mapping(id) in acquisto

--- src/main/java/org/learning/springautomobile/controller/AcquistoController.java
+++ src/main/java/org/learning/springautomobile/controller/AcquistoController.java
@@ -2,19 +2,19 @@ package org.learning.springautomobile.controller;
 
 import jakarta.validation.Valid;
 import org.learning.springautomobile.model.Acquisto;
+import org.learning.springautomobile.model.Auto;
 import org.learning.springautomobile.repository.AcquistoRecovery;
 import org.learning.springautomobile.repository.AutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
-@RequestMapping("home/auto/acquisto")
+@RequestMapping("/acquisto")
 public class AcquistoController {
 
     @Autowired
@@ -29,14 +29,13 @@ public class AcquistoController {
         return "";
     }
 
-    @GetMapping("/create")
-    public String createAcquisto(Model model){
-        Acquisto acquisto = new Acquisto();
-        model.addAttribute("acquisto",acquisto);
+    @GetMapping("/create/{id}")
+    public String createAcquisto(@PathVariable int id , Model model){
+        Optional<Auto> auto = autoRepository.findById(id);
         return "";
     }
 
-    @PostMapping("/create")
+    @PostMapping("/create/{id}")
     public String saveAcquisto(@Valid @ModelAttribute("acquisto") Acquisto formAcquisto, BindingResult bindingResult){
         return "";
     }
