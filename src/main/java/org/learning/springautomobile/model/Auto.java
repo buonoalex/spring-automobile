73d95672ea1c4f4127e2462ac8f91b82158e86f9
buonoalex <loopatthecity@gmail.com>
2024-01-29 17:55:33 +0100

    finish session

--- src/main/java/org/learning/springautomobile/model/Auto.java
+++ src/main/java/org/learning/springautomobile/model/Auto.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
